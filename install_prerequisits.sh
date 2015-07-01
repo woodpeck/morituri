e540ffc0674cc88141c3de2524d2a33855c4004a
Philip Beelmann <philip@geofabrik.de>
2015-07-01 10:18:25 +0200

    updates install instructions

--- install_prerequisits.sh
+++ install_prerequisits.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo apt-get -y install libbz2-dev libgdal-dev libexpat1-dev libgeos++-dev libpthread-stubs0-dev zlib1g-dev libosmpbf-dev libprotobuf-dev libboost-dev libboost-filesystem-dev
-
-sudo apt-get -y install gdal-bin
-
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install gcc-4.9
+
+sudo apt-get -y install libbz2-dev libgdal-dev libexpat1-dev libgeos++-dev libpthread-stubs0-dev zlib1g-dev libosmpbf-dev libprotobuf-dev libboost-dev libboost-filesystem-dev
+
+sudo apt-get -y install gdal-bin
 
 mkdir -p ~/libs
 cd ~/libs
@@ -16,8 +16,11 @@ git clone https://github.com/osmcode/libosmium
 cd ~/libs
 curl http://download.osgeo.org/shapelib/shapelib-1.3.0.tar.gz -o shapelib-1.3.0.tar.gz
 tar xzf shapelib-1.3.0.tar.gz
+rm shapelib-1.3.0.tar.gz
 cd shapelib-1.3.0
 make
 sudo make install
+sudo chmod 644 /usr/local/include/shapefil.h
 
-rm shapelib-1.3.0.tar.gz
+sudo apt-get -y update
+sudo apt-get -y upgrade
